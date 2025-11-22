332f5cb0b23d3f1beed73fbc1d0e47d4f2729891
anamunizzz <2018analuizamunizsousa@gmail.com>
2025-11-22 16:39:10 -0300

    feat: last update

---
+++ src/exercicios/02.js
@@ -0,0 +1,18 @@
+ // let botao = document.getElementById("btn")
+        // botao.onclick = () => {
+        //     let numero = prompt("digite um numero")
+        //     alert(numero + " ana")
+        // }
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    alert("botão foi clicado!")
+})
+
+        
+document.addEventListener("form").addEventListener
+("submit", (event) => {
+    e.preventDefault()
+    console.log('teste')
+})
